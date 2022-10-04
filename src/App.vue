e50bb2dbd8d9a178a69d644e8285a758fd1ab30d
Alejandro Arevalo <am01824@rmn.com>
2022-10-04 12:09:37 +0100

    new font

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,13 @@ import { RouterView } from 'vue-router';
 </script>
 
 <template>
+  <header class="w-full bg-purple-700">
+    <h1
+      class="container text-4xl tracking-widest flex h-16 items-center text-white font-peralta"
+    >
+      OPPLY
+    </h1>
+  </header>
   <main class="container">
     <RouterView />
   </main>
--- src/views/SuppliersListView.vue
+++ src/views/SuppliersListView.vue
@@ -1,4 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useOpplyAPI } from '@/composables/useOpplyAPI';
+import type { Supplier } from '@/types/entities';
+import { onMounted, ref } from 'vue';
+
+const { getSuppliers } = useOpplyAPI();
+const suppliers = ref<Supplier[]>([]);
+
+onMounted(async () => {
+  const { data, error } = await getSuppliers();
+  suppliers.value = data.value?.results || [];
+});
+</script>
 
 <template>
   <main></main>
